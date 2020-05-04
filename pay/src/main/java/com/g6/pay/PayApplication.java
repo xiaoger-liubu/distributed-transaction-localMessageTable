b921346e22882752155f24a001917ed02a2c11d2
xiaogerliubu <xiaogerliubu@163.com>
2020-05-04 20:31:46 +0800

    gitingore文件提交

--- pay/src/main/java/com/g6/pay/PayApplication.java
+++ pay/src/main/java/com/g6/pay/PayApplication.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PayApplication {
     
-    
     public static void main(String[] args){
         SpringApplication.run(PayApplication.class,args);
     }
